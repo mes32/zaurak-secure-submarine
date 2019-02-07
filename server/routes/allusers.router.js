619cfbfc73137bca4f1ee099b4fb7b3f7b743250
mes32 <stockman.mike@gmail.com>
2019-02-07 11:36:19 -0600

    Add router for fetching all users 'allusers.router.js'.

---
+++ server/routes/allusers.router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    console.log('req.user:', req.user);
+    if (req.isAuthenticated()) {
+        const queryText = `
+        SELECT * FROM "person";
+        `;
+        pool.query(queryText, [req.user.clearance_level])
+            .then(results => res.send(results.rows))
+            .catch(error => {
+                console.log('Error making SELECT FROM "person":', error);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+module.exports = router;
