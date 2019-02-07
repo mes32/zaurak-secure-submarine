e7b9246bce053f9c678fcaca5488cc946197e689
mes32 <stockman.mike@gmail.com>
2019-02-07 13:20:51 -0600

    Clean up code in allusers.router.js.

--- server/routes/allusers.router.js
+++ server/routes/allusers.router.js
@@ -4,20 +4,15 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', rejectUnauthenticated, (req, res) => {
-    console.log('req.user:', req.user);
-    if (req.isAuthenticated()) {
-        const queryText = `
-        SELECT id, username, clearance_level AS clearance FROM "person";
-        `;
-        pool.query(queryText)
-            .then(results => res.send(results.rows))
-            .catch(error => {
-                console.log('Error making SELECT FROM "person":', error);
-                res.sendStatus(500);
-            });
-    } else {
-        res.sendStatus(403);
-    }
+    const queryText = `
+    SELECT id, username, clearance_level AS clearance FROM "person";
+    `;
+    pool.query(queryText)
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT FROM "person":', error);
+            res.sendStatus(500);
+        });
 });
 
 module.exports = router;
